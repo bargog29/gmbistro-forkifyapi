c2b3e9da39564dd1c9bfc4387350af798f661f22
bargog29 <baranga.georgemarius@gmail.com>
2023-09-04 13:28:20 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '12c7f308-eced-4187-a2e7-ed40b6e99395';
+export const MODAL_CLOSE_SEC = 2.5;
